81da53344a1cbaa021f2261362d6dba8edfe4793
AishwaryaArun12 <jj4245888@gmail.com>
2022-08-14 15:30:35 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
 const Join = (props) =>{
+  console.log('hai');
   return(
     <div id ="join">
-      {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      {/* Access prop 'words' and print it using .join like words.join(',')*/props.words.join()}
     </div>
   )
 }
 const App = () => {
-
+console.log('hai');
   return (
     <div id="main">
-      <Join  />
+      <Join words={arr} />
     </div>
   )
 }
